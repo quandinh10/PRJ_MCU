ab715146c505cb029cdf6ebe00a10aeb8653dd18
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2023-11-13 14:07:30 +0700

    thinh init

--- src_code/Core/Src/main.c
+++ src_code/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+//branch THINH
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "global.h"
