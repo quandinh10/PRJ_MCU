edfedd53840f47583ac006dec933b96ad38fd0c0
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2023-11-23 11:47:57 +0700

    buzzer2

---
+++ src_code/Core/Inc/buzzer.h
@@ -0,0 +1,17 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Nov 23, 2023
+ *      Author: takudo
+ */
+
+#ifndef INC_BUZZER_H_
+#define INC_BUZZER_H_
+
+#include "global.h"
+#include "main.h"
+
+void startBuzzer();
+
+
+#endif /* INC_BUZZER_H_ */
